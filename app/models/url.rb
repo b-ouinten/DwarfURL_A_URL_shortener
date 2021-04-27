296795f7a394a248cdff4419bebf0c7965c3f5d3
ouin85 <bouinten@yahoo.fr>
2021-04-27 09:43:23 +0200

    override should_generate_new_friendly_id? in order to say to friendly_id gem to generate slug for new records

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,7 +6,11 @@ class Url < ApplicationRecord
   validates :url_content, presence: true, uniqueness: { message: "already exist !" }
   validates :alias, uniqueness: { message: "already exist !" }
 
-  # Extra
+  # Friendly_id gem
   extend FriendlyId
   friendly_id :alias, use: :slugged
+
+  def should_generate_new_friendly_id?
+    new_record?
+  end
 end
