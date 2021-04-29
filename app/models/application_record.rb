4d8545a3a1ac3b3aff8498dad4b8d6fab3221c8e
ouin85 <bouinten@yahoo.fr>
2021-04-29 11:01:05 +0200

    change generate alias method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
+  CHARSET = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a.freeze
+  
   def generate_random_string(size)
-    charset = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a
-    Array.new(size) { charset.sample }.join
+    Array.new(size) { CHARSET.sample }.join
   end
 end
