2b86c896950b175cb118f31ff72e4dc48161266b
ouin85 <bouinten@yahoo.fr>
2021-05-02 11:15:33 +0200

    offline show

--- app/controllers/offline/urls_controller.rb
+++ app/controllers/offline/urls_controller.rb
@@ -2,8 +2,7 @@ class Offline::UrlsController < ApplicationController
   before_action :handle_alias, only: :create
   
   def index
-    cookie = pull_cookie_at :dwarfURLs
-    @urls = cookie.map { UrlClone.new _1 }.reverse
+    @urls = pull_cookie_at(:dwarfURLs).reverse
   end
 
   def new
@@ -14,10 +13,18 @@ class Offline::UrlsController < ApplicationController
     params = permitted_url_params
     params[:_alias] = @alias
     push_to_cookie_at :dwarfURLs, params.to_h
+
     redirect_to offline_my_dwarfURLs_path
   end
 
   def show
+    _alias = params[:id]
+    @urls = pull_cookie_at :dwarfURLs
+    @url = @urls.keep_if do |x|
+      x["_alias"] == _alias
+    end.last
+
+    redirect_to @url["link"]
   end
 
   def destroy
--- app/models/url_clone.rb
+++
@@ -1,8 +0,0 @@
-class UrlClone
-  attr_accessor :link, :_alias
-  
-  def initialize(attributes = {})
-    @link = attributes['link']
-    @_alias = attributes['_alias']
-  end
-end
