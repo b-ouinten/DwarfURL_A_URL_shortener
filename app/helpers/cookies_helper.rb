95e9255a571801b899996a605c59d0d964cd6bc8
ouin85 <bouinten@yahoo.fr>
2021-05-04 11:54:23 +0200

    cookie feature done

--- app/helpers/cookies_helper.rb
+++ app/helpers/cookies_helper.rb
@@ -8,4 +8,10 @@ module CookiesHelper
   def pull_cookie_at(key)
     cookies[key] ? JSON.parse(cookies[key]) : []
   end
+
+  def inject_cookie_urls_to_db
+    urls = pull_cookie_at :dwarfURLs
+    current_user.urls << urls.map { Url.new(_1) }
+    cookies.delete :dwarfURLs
+  end
 end
